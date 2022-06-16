5fa85e79e64418d973fdb7e43f6fa109777df90e
bob <bob@>
2022-06-16 14:22:37 -0400

    shit

---
+++ CREDITS.md
@@ -0,0 +1,5 @@
+# Credits
+
+Vortex - Owner
+Leather128 - Being good at godot and helping me when shit .
+All the tutorials i followed to make this piece of shit happen lmao.
--- source/scripts/Title.gd
+++ source/scripts/Title.gd
@@ -1,30 +1,7 @@
 extends Control
-
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-
 func _on_StartButton_pressed() -> void:
 	get_tree().change_scene("res://source/scenes/MainScene.tscn")
-
-
 func _on_QuitButton_pressed() -> void:
 	get_tree().change_scene("res://source/scenes/QuitConfirmScene.tscn")
-
-
-
-
 func _on_OptionsButton_pressed() -> void:
 	get_tree().change_scene("res://source/scenes/Options.tscn")
